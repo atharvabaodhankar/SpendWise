4503ee012141b56fd5746fa4edd615e915b1197b
Atharva Baodhankar <108174720+atharvabaodhankar@users.noreply.github.com>
2026-02-09 21:20:05 +0530

    feat: Add new API endpoint for sending templated emails via Gmail SMTP, supporting various notification types.

--- api/send-email-gmail.js
+++ api/send-email-gmail.js
@@ -1,7 +1,7 @@
 // Gmail SMTP email service - works with any email address
 const nodemailer = require('nodemailer');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
